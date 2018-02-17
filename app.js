81fd71a04ab5d783ac22d9a428876dfa5c352f32
Smelt <shivamsatyarthi@gmail.com>
2018-02-17 17:35:48 -0500

    added navbar functionality and logout feature

--- app.js
+++ app.js
@@ -134,7 +134,44 @@ app.post("/campgrounds/:id/comments", function(req,res){
 
 });
 
+//auth routes
+app.get("/register", function(req,res){
+    res.render("register")
+})
+
+app.post("/register", function(req,res){
+    let newUser = new User({username: req.body.username})
+   User.register(newUser, req.body.password, function(err, user){
+       if(err){
+           console.log(err);
+           return res.render("register");
+       }
+       passport.authenticate("local")(req, res, function(){
+           res.redirect("/campgrounds")
+       });
+   });
+});
+
+//show login form
+app.get("/login", function(req,res){
+    res.render("login");
+});
+
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }), function (req,res){
+        res.send("Logged in");
+});
+
+//log out rout
+app.get("/logout", function(req,res){
+    req.logout();
+    res.redirect("/campgrounds");
+})
+
 app.listen(3000, function(){
     console.log("Yelp Camp Server has started");
-});
+}); 
 
